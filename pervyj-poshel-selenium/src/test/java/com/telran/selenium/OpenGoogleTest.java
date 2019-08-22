93bd53ee59b9c4bb49c37e44565b7da4c412fff5
Olga-Mar <marina.yasina@gmail.com>
2019-08-22 21:02:20 +0300

    Login To Ebay

--- pervyj-poshel-selenium/src/test/java/com/telran/selenium/OpenGoogleTest.java
+++ pervyj-poshel-selenium/src/test/java/com/telran/selenium/OpenGoogleTest.java
@@ -1,22 +1,39 @@
 package com.telran.selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class OpenGoogleTest {
-    WebDriver driver;
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
+
+public class OpenGoogleTest extends TestBase {
+
     @Test
-    public void setUp() throws InterruptedException {
-       driver= new ChromeDriver();
-       driver.get("https://www.google.com");
-       driver.findElement(By.name("q")).sendKeys("Bobruisk");
-       driver.findElement(By.name("go")).click();
-       Thread.sleep(6000);
-       driver.quit();
 
+    public void searchJavaTest() throws InterruptedException {
+        driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).click();
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("go")).sendKeys("java");
+        driver.findElement(By.name("go")).sendKeys("java"+ Keys.ENTER);
 
+        Thread.sleep(6000);
+    }
+
+    @Test
+public void clickmfillingLucky(){
+       new Actions(driver)
+               .moveToElement(driver.findElement(By.name("btnI"))).click().perform();
+        //Action +perform-vsegda vmeste
+        //
+        driver.findElement(By.name("btnI")).click();
 
     }
+
 }
