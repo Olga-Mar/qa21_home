98660b6840d4b46702a63464381f83b79f520da0
Olga-Mar <marina.yasina@gmail.com>
2019-10-10 13:14:34 +0300

    Logger, add Logs

--- login-wki1/src/test/java/com/wiki/tests/SearchTests1.java
+++ login-wki1/src/test/java/com/wiki/tests/SearchTests1.java
@@ -1,13 +1,19 @@
 package com.wiki.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SearchTests1 extends TestBase {
 
 
+
     @Test(priority = 2)
     public void searchWikiTestJava() {
+
         TestBase.app.getArticle().typeRequest("java");
         TestBase.app.getArticle().clickGoButton();
         Assert.assertEquals(TestBase.app.getArticle().getArticleTitle().toLowerCase(), "java".toLowerCase());
--- login-wki1/src/test/java/com/wiki/tests/TestBase.java
+++ login-wki1/src/test/java/com/wiki/tests/TestBase.java
@@ -2,11 +2,30 @@ package com.wiki.tests;
 
 import com.wiki.fw.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 
 public class TestBase {
+    Logger logger= LoggerFactory.getLogger(TestBase.class);
+
+    @BeforeMethod
+    public  void startLogger(Method m){
+        logger.info("start test "+ m.getName());
+
+    }
+    @AfterMethod
+    public  void stopLogger(Method m){
+        logger.info("stop test "+ m.getName());
+        System.out.println("*********************************************");
+
+    }
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
